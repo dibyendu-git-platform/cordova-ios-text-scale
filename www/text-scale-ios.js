c494e7c602000f2504a06397b9e944359e774345
Dibyendu Debnath <dibyendu.nir@gmail.com>
2025-04-08 15:43:58 +0530

    Delete www/text-scale-ios.js

--- www/text-scale-ios.js
+++
@@ -1,9 +0,0 @@
-var exec = require('cordova/exec');
-
-var TextScale = {
-    getTextScale: function(successCallback, errorCallback) {
-        exec(successCallback, errorCallback, "TextScale", "getTextScale", []);
-    }
-};
-
-module.exports = TextScale;
